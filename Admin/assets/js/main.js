f04ae34269a614c35bd0ed6f677e7d4643772eb1
Tuong Pham <tuongpvth@gmail.com>
2017-08-22 17:10:01 +0700

    Add file to Admin Folder

---
+++ Admin/assets/js/main.js
@@ -0,0 +1,7 @@
+angular.module("ungdungdautien", [])
+    .controller('demoController', function ($scope) {
+        $scope.info = {
+            CourseName: "AngularJS",
+            Author: "Tuongpv"
+        };
+    })
